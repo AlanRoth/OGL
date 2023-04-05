830f5821dcfc0fcb0c1c57d4252104cc8e706f7a
AlanRoth <asroth10@gmail.com>
2023-04-05 12:36:25 +0100

    Recreate public folder, begin implementation

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,11 +1,25 @@
 import './assets/css/App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="app">
-
+      <MyButton />
     </div>
   );
 }
+
+function ProductRow({product}) {
+  return (
+      <tr>
+        <td>{product.sku}</td>
+        <td>{product.price}</td>
+      </tr>
+  )
+}
+
+const PRODUCTS = [
+    {}
+]
 
 export default App;
