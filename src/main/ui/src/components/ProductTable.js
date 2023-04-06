7e82e57f39d5aad1d5daf46857e5bd0c40252430
AlanRoth <alan.roth@payara.fish>
2023-04-06 20:49:40 +0100

    Move ProductTable into seperate component. Add description to schema.

---
+++ src/main/ui/src/components/ProductTable.js
@@ -0,0 +1,32 @@
+function ProductTable({products}) {
+    const rows = [];
+
+    products.forEach((product) => rows.push(<ProductRow product={product} key={product.id} />))
+
+    return(
+        <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>SKU</th>
+                <th>PRICE</th>
+                <th>DESCRIPTION</th>
+            </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
+    )
+}
+
+function ProductRow({product}) {
+    return (
+        <tr>
+            <td>{product.id}</td>
+            <td>{product.sku}</td>
+            <td>{product.price}</td>
+            <td>{product.description}</td>
+        </tr>
+    )
+}
+
+export default ProductTable;
